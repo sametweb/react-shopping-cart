19598928791aa2ac9772d8fa157d5a2d1fa597ba
sametweb <sametmutevellioglu@gmail.com>
2020-01-28 21:32:12 -0800

    cart data persists through useLocalStorage custom hook

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
 
+//custom hooks
+import { useLocalStorage } from "./hooks/useLocalStorage";
+
 //contexts
 import { ProductContext } from "./contexts/ProductContext";
 import { CartContext } from "./contexts/CardContext";
@@ -14,21 +17,25 @@ import ShoppingCart from "./components/ShoppingCart";
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
+  const [storedCart, setStoredCart] = useLocalStorage("cart");
 
   const addItem = item => setCart([...cart, item]);
 
   const removeItem = index =>
     setCart(cart.filter((item, itemIndex) => itemIndex !== index));
 
-  const storeIt = (key, value) =>
-    window.localStorage.setItem(key, JSON.stringify(value));
+  useEffect(() => setCart(JSON.parse(storedCart)), [storedCart]); //Populates cart if there is data in localStorage
+  useEffect(() => setStoredCart(cart), [cart, setStoredCart]); //Saves in localStorage everytime cart changes
 
-  useEffect(() => {
-    const savedCart = window.localStorage.getItem("cart");
-    if (savedCart) setCart(JSON.parse(savedCart));
-  }, []);
+  //   const storeIt = (key, value) =>
+  //     window.localStorage.setItem(key, JSON.stringify(value));
 
-  useEffect(() => storeIt("cart", cart), [cart]);
+  //   useEffect(() => {
+  //     const savedCart = window.localStorage.getItem("cart");
+  //     if (savedCart) setCart(JSON.parse(savedCart));
+  //   }, []);
+
+  //   useEffect(() => storeIt("cart", cart), [cart]);
 
   console.log(cart);
   return (
